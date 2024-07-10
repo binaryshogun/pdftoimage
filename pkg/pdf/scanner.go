3337a432d53f92954c6e3a6da9602ef90697284d
Alexander Karpovich <alexander.lomonosov.03@gmail.com>
2024-07-11 00:34:01 +0300

    implement PDF to JPG and PNG conversion

---
+++ pkg/pdf/scanner.go
@@ -0,0 +1,48 @@
+package pdf
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gen2brain/go-fitz"
+	"image"
+)
+
+// Scanner - scanner for PDF documents.
+type Scanner struct{}
+
+// NewScanner creates new instance of *[Scanner].
+func NewScanner() *Scanner {
+	return &Scanner{}
+}
+
+// Scan scans the PDF file and converts it to the slice of images.
+func (s *Scanner) Scan(path string) (images []image.Image, err error) {
+	// Open the PDF file
+	var doc *fitz.Document
+	doc, err = fitz.New(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open PDF file: %w", err)
+	}
+	defer func(doc *fitz.Document) {
+		closeErr := doc.Close()
+		if closeErr != nil {
+			err = errors.Join(err, fmt.Errorf("failed to close PDF file: %w", closeErr))
+		}
+	}(doc)
+
+	// Allocate space for images.
+	images = make([]image.Image, doc.NumPage())
+
+	// Iterate through each page and convert it to an image
+	for pageNumber := 0; pageNumber < doc.NumPage(); pageNumber++ {
+		var img image.Image
+		img, err = doc.Image(pageNumber)
+		if err != nil {
+			return nil, fmt.Errorf("could not get image from page %d: %v", pageNumber, err)
+		}
+
+		images[pageNumber] = img
+	}
+
+	return nil, nil
+}
